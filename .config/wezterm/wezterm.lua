ce87f82e676ed251a074fadc8e567622aa636a0f
Samir Arioui <arioui.samir@gmail.com>
2024-11-08 16:58:20 +0100

    increase wezterm font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,5 +7,5 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.color_scheme = "catppuccin-mocha"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 14
+config.font_size = 18
 return config
